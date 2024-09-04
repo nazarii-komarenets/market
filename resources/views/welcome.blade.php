db661b9c65769c6269b9beb40f7ac466c1b6ac34
nazarii-komarenets <komarenecn@gmail.com>
2024-09-04 18:17:06 +0300

    first push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.default')
 
 @section('content')
-    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+    <div class="bg-gray-50 text-black/50">
         @livewire('list-products')
     </div>
 @endsection
