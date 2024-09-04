ceda1215158e71e8d2b3833542cc6d9c07910184
nazarii-komarenets <komarenecn@gmail.com>
2024-09-04 18:14:54 +0300

    first push

--- resources/views/components/footer_bottom.blade.php
+++ resources/views/components/footer_bottom.blade.php
@@ -1,3 +1,3 @@
-<div class="max-w-screen-xl items-center py-5 m-auto bg-gray-100 text-gray-800">
-    <div class="my-5 w-full text-center">© WahaMarket 2024</div>
+<div class="w-full items-center py-5 m-auto bg-gray-100 text-gray-800">
+    <div class="my-5 max-w-screen-xl mx-auto text-center">© WahaMarket 2024</div>
 </div>
