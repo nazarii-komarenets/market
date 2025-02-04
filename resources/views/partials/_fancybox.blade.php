a35fda3558d5e621fe1c0ebeee440d67fa8673cc
nazarii-komarenets <komarenecn@gmail.com>
2025-02-04 17:07:57 +0200

    Split load Fancy box

---
+++ resources/views/partials/_fancybox.blade.php
@@ -0,0 +1,9 @@
+<!-- Fancybox CSS -->
+<link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css"
+/>
+
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+@endpush
