36e38ae710046e4b7a2bcc467d9025bf0b36e6e7
nazarii-komarenets <komarenecn@gmail.com>
2025-02-05 11:58:06 +0200

    Add Seo main text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,4 +8,8 @@
     <div class="bg-gray-50 text-black/50">
         @livewire('list-products')
     </div>
+
+    <div class="my-5">
+        @include('partials.seo_main_text')
+    </div>
 @endsection
